c96812289d1e5e89661c964dab630da2f49d063a
Carlos <133175569+Rcarlohh@users.noreply.github.com>
2023-12-09 21:44:16 -0600

    first commit

--- src/store/index.js
+++ src/store/index.js
@@ -4,28 +4,19 @@ const store = createStore({
   state() {
     return {
       memories: [
-        {
-          id: "m1",
-          image:
-            "https://www.ticcihmexico.org/wp-content/uploads/2016/06/1-Mina-de-Acosta-MAHB-2-768x576.jpg",
-          title: "Mineral del monte",
-          description: "Montañas",
-        },
-        {
-          id: "m2",
-          image:
-            "https://i.pinimg.com/474x/eb/a4/39/eba439ea69746ed229f9e517619cbd7f.jpg",
-          title: "Tuzoofari Pachuca",
-          description: "Montañas",
-        },
-        {
-          id: "m3",
-          image:
-            "https://www.jeskat.com.mx/wp-content/uploads/2021/03/207-Villa-del-Carbon-1-768x576.jpg",
-          title: "Villa del carbon",
-          description: "Montañas",
-        },
-      ],
+        {id: 'm1', image:'../public/images/cruzaxul.jpg',
+        title:'Cruz Azul Campeon', description:'Simplemente Cruz AZUL Campeon'},
+        
+        {id: 'm2', image:'../public/images/conoci.jpg', 
+        title:'El Dia Que Te Conoci', description:'Podria ser este mi ser amado'},
+        
+        {id: 'm3', image:'../public/images/santo.jpg', 
+        title:'Mi Ultimo Cumple', description:'MI Santo'},
+        
+        {id: 'm4', image:'../public/images/folk.jpg', 
+        title:'Presentacion Folklor', description:'Zapateado Endiablado'},
+
+    ],
     };
   },
   mutations: {
